93e590cdab556f52abb80ca2d2fecca4faffb959
Ryan Doherty <tech@conical.org>
2019-01-04 04:25:22 +0000

    Move beans to WDKWebsite and use 'real' WdkModel and User objects in ServletContext and HttpSession respectively
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDKWebsite/trunk@23633 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Controller/src/main/java/org/gusdb/wdk/controller/actionutil/ActionUtility.java
+++ Controller/src/main/java/org/gusdb/wdk/controller/actionutil/ActionUtility.java
@@ -8,14 +8,11 @@ import java.util.Map.Entry;
 import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
-import org.gusdb.wdk.controller.WdkInitializer;
 import org.gusdb.wdk.model.Utilities;
 import org.gusdb.wdk.model.WdkRuntimeException;
 import org.gusdb.wdk.model.jspwrap.UserBean;
 import org.gusdb.wdk.model.jspwrap.WdkModelBean;
-import org.gusdb.wdk.model.user.User;
 
 /**
  * Heritage methods to access the current user,  model, and request params
@@ -26,12 +23,11 @@ public class ActionUtility {
 
     public static UserBean getUser(HttpServletRequest request) {
         try {
-            HttpSession session = request.getSession();
-            User user = (User)session.getAttribute(Utilities.WDK_USER_KEY);
+            UserBean user = (UserBean)request.getSession().getAttribute(Utilities.WDK_USER_BEAN_KEY);
             if (user == null) {
               throw new IllegalStateException("No user present on session. This should never happen.");
             }
-            return new UserBean(user);
+            return user;
         }
         catch (Exception ex) {
             throw new WdkRuntimeException(ex);
@@ -39,7 +35,7 @@ public class ActionUtility {
     }
 
     public static WdkModelBean getWdkModel(HttpServlet servlet) {
-        return new WdkModelBean(WdkInitializer.getWdkModel(servlet.getServletContext()));
+        return (WdkModelBean)servlet.getServletContext().getAttribute(Utilities.WDK_MODEL_BEAN_KEY);
     }
 
     public static Map<String, String> getParams(ServletRequest request) {
