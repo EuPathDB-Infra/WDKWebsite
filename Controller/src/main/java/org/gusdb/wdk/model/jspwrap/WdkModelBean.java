a41c1b551a384a256d13155b758d2ab2d9bd6a78
Ryan Doherty <tech@conical.org>
2019-02-10 19:50:21 +0000

    Clean up WdkModelBean and add required method
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDKWebsite/branches/strategy-loading@23908 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Controller/src/main/java/org/gusdb/wdk/model/jspwrap/WdkModelBean.java
+++ Controller/src/main/java/org/gusdb/wdk/model/jspwrap/WdkModelBean.java
@@ -1,7 +1,5 @@
 package org.gusdb.wdk.model.jspwrap;
 
-import java.sql.Connection;
-import java.sql.SQLException;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -9,7 +7,7 @@ import org.gusdb.wdk.model.Reference;
 import org.gusdb.wdk.model.WdkModel;
 import org.gusdb.wdk.model.WdkModelException;
 import org.gusdb.wdk.model.WdkUserException;
-import org.gusdb.wdk.model.dbms.ConnectionContainer;
+import org.gusdb.wdk.model.record.RecordClass;
 import org.gusdb.wdk.model.user.User;
 import org.gusdb.wdk.model.xml.XmlQuestionSet;
 import org.gusdb.wdk.model.xml.XmlRecordClassSet;
@@ -18,7 +16,7 @@ import org.gusdb.wdk.model.xml.XmlRecordClassSet;
  * A wrapper on a {@link WdkModel} that provides simplified access for
  * consumption by a view
  */
-public class WdkModelBean implements ConnectionContainer {
+public class WdkModelBean {
 
     WdkModel wdkModel;
 
@@ -49,6 +47,10 @@ public class WdkModelBean implements ConnectionContainer {
     // to do: figure out how to do this without using getModel()
     public WdkModel getModel() {
         return wdkModel;
+    }
+
+    public RecordClass[] getRecordClasses() {
+      return wdkModel.getAllRecordClasses().toArray(new RecordClass[0]);
     }
 
     public XmlQuestionSetBean[] getXmlQuestionSets() {
@@ -89,15 +91,6 @@ public class WdkModelBean implements ConnectionContainer {
         return wdkModel.getProjectId();
     }
 
-    /**
-     * @param paramName
-     * @return
-     * @see org.gusdb.wdk.model.WdkModel#queryParamDisplayName(java.lang.String)
-     */
-    public String queryParamDisplayName(String paramName) {
-        return wdkModel.queryParamDisplayName(paramName);
-    }
-
     public String getSecretKey() {
         return wdkModel.getModelConfig().getSecretKey();
     }
@@ -116,12 +109,6 @@ public class WdkModelBean implements ConnectionContainer {
      */
     public String getReleaseDate() {
         return wdkModel.getReleaseDate();
-    }
-
-    @Override
-    public Connection getConnection(String key) 
-        throws WdkModelException, SQLException {
-      return wdkModel.getConnection(key);
     }
 
     /**
@@ -151,40 +138,6 @@ public class WdkModelBean implements ConnectionContainer {
           Reference r = new Reference(qFullName);
           XmlQuestionSet xqs = wdkModel.getXmlQuestionSet(r.getSetName());
           xqs.getQuestion(r.getElementName());
-        }
-        catch (WdkModelException e2) {
-          throw new WdkUserException(message, e2);
-        }
-      }
-    }
-
-    /**
-     * Checks for a valid record class name and throws WdkUserException if param
-     * is not valid.  For now we simply check that it is a valid two-part name
-     * (i.e. \S+\.\S+), so we will still get a WdkModelException down the line
-     * if the record class name is the correct format but does not actually
-     * exist.  We do this because sometimes developers change record class names
-     * in one place but not another and if so, then we want to know about it.
-     * If we mask this mistake with a WdkUserException, we might see bad
-     * consequences down the line.
-     * 
-     * @param recordClassName potential record class name
-     * @throws WdkUserException if name is not in format *.*
-     */
-    public void validateRecordClassName(String recordClassName) throws WdkUserException {
-      String message = "The record type '" + recordClassName + "' is not or is no longer available.";
-      try {
-        // First check to see if this is a 'regular' record class; if not, check XML record classes
-        if (recordClassName == null || wdkModel.getRecordClassByName(recordClassName) == null) {
-          throw new WdkModelException("RecordClass name is null or resulting RecordClass is null");
-        }
-      }
-      catch (WdkModelException e) {
-        try {
-          // exception will be thrown below; will mean that name is neither 'regular' RC nor XML
-          Reference r = new Reference(recordClassName);
-          XmlRecordClassSet xrcs = wdkModel.getXmlRecordClassSet(r.getSetName());
-          xrcs.getRecordClass(r.getElementName());
         }
         catch (WdkModelException e2) {
           throw new WdkUserException(message, e2);
