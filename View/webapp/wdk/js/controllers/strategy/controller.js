f1082ee326b7868f0de6943f36965c392887c635
Dave Falke <dmfalke@gmail.com>
2019-03-26 20:37:09 +0000

    refresh strategy panel when filters are modified
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDKWebsite/trunk@24437 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/controllers/strategy/controller.js
+++ View/webapp/wdk/js/controllers/strategy/controller.js
@@ -30,7 +30,8 @@ wdk.namespace("window.wdk.strategy.controller", function (ns, $) {
   // Current strategy and step objects
   var uiState = {
     strategy: null,
-    step: null
+    step: null,
+    isBoolean: null
   };
 
   function setUIState(newState) {
@@ -123,9 +124,39 @@ wdk.namespace("window.wdk.strategy.controller", function (ns, $) {
         $detailBox.find('.analyze_step_link').removeClass('disabled');
       });
 
-    // update state when analysis status changes
-    $(document).on('analysis:statuschange', function() {
-      fetchStrategies(mergeStrategies);
+    // FIXME Factor out into own function
+    const { store } = window.ebrc.context;
+    let prevState = store.getState();
+    store.subscribe(() => {
+      const nextState = store.getState();
+      const selectedStepId = uiState.step && (
+        uiState.isBoolean ? uiState.step.back_boolean_Id : uiState.step.back_step_Id );
+      const prevStep = prevState.steps.steps[selectedStepId];
+      const nextStep = nextState.steps.steps[selectedStepId];
+      const stepChanged = (
+        // if we're just getting the step, don't consider it changed
+        prevStep != null && nextStep != null &&
+        // use deep equality check since we clear the step cache when results are reloaded
+        !_.isEqual(prevStep.answerSpec, nextStep.answerSpec)
+      );
+
+      // TODO Refine detection so that only updated to saved analyses are handled
+      const prevAnalysisState = prevState.stepAnalysis.analysisPanelStates;
+      const nextAnalysisState = nextState.stepAnalysis.analysisPanelStates;
+      const analysesChanged = prevAnalysisState !== nextAnalysisState;
+
+      if (stepChanged) {
+        // update the strategy panel, but don't reload the results panel
+        fetchStrategies(_.partial(updateStrategies, _, true, false));
+        console.log('Detected step updated in redux store', { prevStep, nextStep });
+      }
+
+      else if (analysesChanged) {
+        fetchStrategies(mergeStrategies);
+        console.log('Detected anlysisState updated in redux store', { prevAnalysisState, nextAnalysisState });
+      }
+
+      prevState = nextState
     });
 
     // Add delegate submit handler here for question form
@@ -330,10 +361,10 @@ wdk.namespace("window.wdk.strategy.controller", function (ns, $) {
    * @param {Boolean} ignoreFilters If `true` filters will not be reloaded.
    *   Otherwise they will be reloaded.
    */
-  function updateStrategies(data, ignoreFilters = false, count = 1) {
+  function updateStrategies(data, ignoreFilters = false, reloadResultPanel = true, count = 1) {
     // Increment count if we refetch strategies with updated results
     var nextUpdateStrategies = _.partialRight(updateStrategies, ignoreFilters,
-                                              count + 1);
+                                              reloadResultPanel, count + 1);
 
     // Fetch strategies with updated results. Used if root step results == -1
     var updateResults = _.partial(fetchStrategies, nextUpdateStrategies, {
@@ -391,7 +422,7 @@ wdk.namespace("window.wdk.strategy.controller", function (ns, $) {
       }
     }
 
-    showStrategies(data.currentView, ignoreFilters, data.state.length);
+    showStrategies(data.currentView, ignoreFilters, reloadResultPanel, data.state.length);
   }
 
   // Use this to sync server objects with client objects without redrawing
@@ -493,7 +524,7 @@ wdk.namespace("window.wdk.strategy.controller", function (ns, $) {
    *  updating strategies. This adds complexity and will probably removed in
    *  favor of event triggering.
    */
-  function showStrategies(view, ignoreFilters, count, deferred){
+  function showStrategies(view, ignoreFilters, reloadResultPanel, count, deferred){
     $("#tab_strategy_results font.subscriptCount").text("(" + count + ")");
     var sC = 0;
     for (var s in ns.strats) {
@@ -531,6 +562,9 @@ wdk.namespace("window.wdk.strategy.controller", function (ns, $) {
         wrapper.height($("#Strategies").height() + 10);
       }
     }
+
+    if (reloadResultPanel === false) return;
+
     if (view.action !== undefined) {
       if (view.action == "share" || view.action == "save") {
         var x = $("a#" + view.action + "_" + view.actionStrat);
@@ -748,11 +782,6 @@ wdk.namespace("window.wdk.strategy.controller", function (ns, $) {
 
     wdk.addStepPopup.current_Front_Strategy_Id = f_strategyId;
 
-    // Hack to make sure we load the latest changes to step in
-    // the new react result panel. This will break if WdkService
-    // changes step caching strategy.
-    wdk.getWdkService()._stepMap.clear();
-
     var strategy = wdk.strategy.model.getStrategy(f_strategyId);
     var step = strategy.getStep(f_stepId, true);
     var url = "showSummary.do";
@@ -835,9 +864,15 @@ wdk.namespace("window.wdk.strategy.controller", function (ns, $) {
 
           setUIState({
             strategy: strategy,
-            step: step
+            step: step,
+            isBoolean: isBoolean
           });
         }
+
+        // Hack to make sure we load the latest changes to step in
+        // the new react result panel. This will break if WdkService
+        // changes step caching strategy.
+        wdk.getWdkService()._stepMap.clear();
 
         wdk.util.removeLoading(f_strategyId);
         wdk.basket.checkPageBasket();
