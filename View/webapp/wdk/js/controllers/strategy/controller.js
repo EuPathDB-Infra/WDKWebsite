266faf69965ef9569985399b2c5c593e449073c9
Dave Falke <dmfalke@gmail.com>
2019-04-09 17:44:33 +0000

    fix reference error
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WDKWebsite/trunk@24592 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- View/webapp/wdk/js/controllers/strategy/controller.js
+++ View/webapp/wdk/js/controllers/strategy/controller.js
@@ -1347,8 +1347,8 @@ wdk.namespace("window.wdk.strategy.controller", function (ns, $) {
     if (prevPanelState.length < nextPanelState.length) return false;
     // Tab removed, check if removed tab is a saved tab
     if (prevPanelState.length > nextPanelState.length) {
-      const removedSaved = _.differenceBy(prevPanelState, nextPanelState, tab => tab.analysisConfig.analysisId);
-      return removedSaved.some(tab => tab.analysisConfig.analysisId);
+      const removedSaved = _.differenceBy(prevPanelState, nextPanelState, tab => _.get(tab, 'analysisConfig.analysisId'));
+      return removedSaved.some(tab => _.get(tab, 'analysisConfig.analysisId'));
     }
     // We don't currently allow reordering, so we can simply compare states by index
     for (let i = 0; i < prevPanelState.length; i++) {
